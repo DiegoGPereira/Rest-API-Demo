7f00ada9baba1fbed1352a65690fcfafa13ede87
Diego <diiegopereiraa_@hotmail.com>
2021-05-27 11:42:55 -0300

    first commit

---
+++ src/main/java/com/diego/repository/ClienteRepository.java
@@ -0,0 +1,11 @@
+package com.diego.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.diego.entities.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long>{
+	
+}
